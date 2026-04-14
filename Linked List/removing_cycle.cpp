bf9989a1798a2cc8772a4d89e3af4fb45d9d8941
VedK5643 <vedagyagupta@gmail.com>
2026-04-14 22:04:58 +0530

    Linked list problems

---
+++ Linked List/removing_cycle.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* slow=head;
+        ListNode* fast=head;
+        bool cycle=false;
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast){
+                cycle=true;
+                break;
+            }
+        }
+        if(!cycle){
+            return NULL;
+        }
+        else{
+            slow=head;
+            ListNode* prev=NULL;
+            while(slow!=fast){
+                slow=slow->next;
+                prev=fast;
+                fast=fast->next;
+            }
+            prev->next=NULL;
+        }
+        
+    }
+};
