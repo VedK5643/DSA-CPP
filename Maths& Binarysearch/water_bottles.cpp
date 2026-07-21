58b0a11c8918aadaee73aeb8cfcab5aef328f8c7
VedK5643 <vedagyagupta@gmail.com>
2026-07-21 23:53:43 +0530

    water bottles

---
+++ Maths& Binarysearch/water_bottles.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+
+
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int total = numBottles;
+        int empty = numBottles;
+        while (empty >= numExchange) {
+            int newBottles = empty / numExchange;
+            total += newBottles;
+            empty = empty % numExchange + newBottles;
+        }
+        return total;
+    }
+};
