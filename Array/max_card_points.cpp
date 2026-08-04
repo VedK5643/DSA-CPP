8acb19ac348292923c1a9ec695a320f28944d39a
VedK5643 <vedagyagupta@gmail.com>
2026-08-05 01:03:49 +0530

    Maximum card points

---
+++ Array/max_card_points.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxScore(vector<int>& cardPoints, int k) {
+        int n = cardPoints.size();
+        int windowSize = n - k;
+        int totalSum = 0;
+        for (int x : cardPoints)
+            totalSum += x;
+
+        if (windowSize == 0)
+            return totalSum; 
+        int windowSum = 0;
+        for (int i = 0; i < windowSize; i++)
+            windowSum += cardPoints[i];
+
+        int minWindowSum = windowSum;
+        for (int i = windowSize; i < n; i++) {
+            windowSum += cardPoints[i] - cardPoints[i - windowSize];
+            minWindowSum = min(minWindowSum, windowSum);
+        }
+
+        return totalSum - minWindowSum;
+    }
+};
