5bff6064de3f11891e7beab5eda5c7d96d34ee6e
VedK5643 <vedagyagupta@gmail.com>
2026-08-05 23:49:42 +0530

    Gray code

---
+++ Maths& Binarysearch/gray_code.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+#include<cmath>
+using namespace std;
+
+
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        vector<int> ans;
+        for(int i=0;i<pow(2,n);i++){
+            ans.push_back(i^(i>>1));
+        }
+        return ans;
+    }
+};
