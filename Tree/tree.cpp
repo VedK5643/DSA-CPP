dd500121d745882ad72f18955ff30eea7dbed26a
VedK5643 <vedagyagupta@gmail.com>
2026-08-11 23:40:18 +0530

    in , post, level order traversal

--- Tree/tree.cpp
+++ Tree/tree.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<queue>
 using namespace std;
 
 class Node{
@@ -37,11 +38,58 @@ void pretraversal(Node* root){
     pretraversal(root->right);
 }
 
+void intraversal(Node* root){
+    if(root == NULL){
+        return;
+    }
+
+    intraversal(root->left);
+    cout<<root->data<<" ";
+    intraversal(root->right);
+}
+
+void posttraversal(Node* root){
+    if(root==NULL){
+        return;
+    }
+
+    posttraversal(root->left);
+    posttraversal(root->right);
+    cout<<root->data<<" ";
+}
+
+void levelorder(Node* root){
+    queue<Node*> q;
+    q.push(root);
+
+    while(q.size()>0){
+        Node* curr=q.front();
+        q.pop();
+
+        cout<<curr->data<<" ";
+
+        if(curr->left!=NULL){
+            q.push(curr->left);
+        }
+        if(curr->right!=NULL){
+            q.push(curr->right);
+        }
+    }
+    cout<<endl;
+
+}
 
 int main(){
     vector<int> preorder={1,2,-1,-1,3,4,-1,-1,5,-1,-1};
     Node* root=buildtree(preorder);
     pretraversal(root);
-    
+    cout<<endl;
+    intraversal(root);
+    cout<<endl;
+    posttraversal(root);
+    cout<<endl;
+    levelorder(root);
+
+
     return 0;
 }
