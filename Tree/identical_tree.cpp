8af4059604de3efbf09c5b5c86f62d09f6c15116
VedK5643 <vedagyagupta@gmail.com>
2026-08-16 22:31:39 +0530

    identical trees

---
+++ Tree/identical_tree.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+right(right) {}
+};
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+
+        if (p == NULL || q == NULL) {
+            return p == q;
+        }
+
+        bool islsame = isSameTree(p->left, q->left);
+        bool isrsame = isSameTree(p->right, q->right);
+        return islsame && isrsame && p->val == q->val;
+    }
+};
