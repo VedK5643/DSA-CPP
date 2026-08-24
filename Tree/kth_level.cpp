a08d597754ff6ba3cb425b4bb8287d08d359957c
VedK5643 <vedagyagupta@gmail.com>
2026-08-25 02:12:30 +0530

    Kth level of binary tree

---
+++ Tree/kth_level.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data=val;
+        left=right=NULL;
+    }
+};
+
+static int idx=-1;
+Node* buildtree(vector<int> preorder){
+    idx++;
+    if(preorder[idx]==-1){
+        return NULL;
+    }
+
+    Node* root= new Node(preorder[idx]);
+    root->left=buildtree(preorder);
+    root->right=buildtree(preorder);
+    return root;
+}
+
+void Kthlevel(Node* root, int k){
+    if(root==NULL){
+        return;
+    }
+    if(k==1){
+        cout<<root->data<<"\t";
+        return;
+    }
+    Kthlevel(root->left,k-1);
+    Kthlevel(root->right,k-1);
+}
+
+
+int main(){
+    vector<int> preorder={1,2,7,-1,-1,-1,3,4,-1,-1,5,-1,-1};
+    Node* root=buildtree(preorder);
+    Kthlevel(root,3);
+
+    return 0;
+}
--- Tree/levelorder.cpp
+++ Tree/levelorder.cpp
@@ -49,3 +49,36 @@ public:
         return levelorder;
     }
 };
+
+
+/* Updated
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> result;
+        if (root == nullptr) return result;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            int n = q.size();
+            vector<int> level;
+
+            for (int i = 0; i < n; i++) {
+                TreeNode* curr = q.front();
+                q.pop();
+                level.push_back(curr->val);
+
+                if (curr->left != nullptr) q.push(curr->left);
+                if (curr->right != nullptr) q.push(curr->right);
+            }
+
+            result.push_back(level);
+        }
+
+        return result;
+    }
+};
+
+*/
